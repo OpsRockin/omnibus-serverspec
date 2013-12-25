41ae53dc9cb1b856a71dedbc66989abddfc5a0ea
sawanoboly <sawanoboriyu@higanworks.com>
2013-12-25 14:34:55 +0900

    use full path for centos

--- config/software/serverspec.rb
+++ config/software/serverspec.rb
@@ -14,6 +14,6 @@ env = {
 }
 
 build do
-  command "sudo gem install serverspec --no-ri --no-rdoc --version=#{gem_version}", :env => env
+  command "sudo #{install_dir}/embedded/bin/gem install serverspec --no-ri --no-rdoc --version=#{gem_version}", :env => env
   command "sudo ln -fs #{install_dir}/embedded/bin/serverspec-init #{install_dir}/bin/serverspec-init", :env => env
 end
