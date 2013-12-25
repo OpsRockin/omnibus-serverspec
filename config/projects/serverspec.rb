2a57f25854a29a1804303c27ee7fd7a66c7497cb
sawanoboly <sawanoboriyu@higanworks.com>
2013-12-25 12:25:22 +0900

    set version 0.14.1

--- config/projects/serverspec.rb
+++ config/projects/serverspec.rb
@@ -5,7 +5,7 @@ homepage "CHANGEME.com"
 
 replaces        "serverspec"
 install_path    "/opt/serverspec"
-build_version   Omnibus::BuildVersion.new.semver
+build_version   '0.14.1-01'
 build_iteration 1
 
 # creates required build directories
