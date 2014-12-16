ee371537965ed46745125bde180ff2018e2a50ae
sawanoboly <sawanoboriyu@higanworks.com>
2014-12-16 17:06:10 +0900

    [ci skip] branch for stable release automaticaly

--- ci/circle_release.sh
+++ ci/circle_release.sh
@@ -12,4 +12,15 @@ if [ "${CIRCLE_BRANCH}" == "master" ]; then
       bundle exec package_cloud push omnibus-serverspec/dummy_with_ci/ubuntu/trusty pkg/*.deb
       ;;
   esac
+elif [ "${CIRCLE_BRANCH}" == "release_package" ]; then
+  case $CIRCLE_NODE_INDEX in
+    0) TARGET="centos"
+      bundle exec kitchen destroy ${TARGET}
+      bundle exec package_cloud push omnibus-serverspec/serverspec/el/6 pkg/*.rpm
+      ;;
+    1) TARGET="ubuntu"
+      bundle exec kitchen destroy ${TARGET}
+      bundle exec package_cloud push omnibus-serverspec/serverspec/ubuntu/trusty pkg/*.deb
+      ;;
+  esac
 fi
