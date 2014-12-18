e0595eb8963ddcd75727b4c17b04efbd05c40278
sawanoboly <sawanoboriyu@higanworks.com>
2014-12-18 20:10:43 +0900

    [ci skip] change push target repo when all

--- ci/circle_release_all.sh
+++ ci/circle_release_all.sh
@@ -3,5 +3,5 @@
 set -e
 
 bundle exec kitchen destroy
-bundle exec package_cloud push omnibus-serverspec/dummy_with_ci/el/6 pkg/*.rpm
-bundle exec package_cloud push omnibus-serverspec/dummy_with_ci/ubuntu/trusty pkg/*.deb
+bundle exec package_cloud push omnibus-serverspec/serverspec/el/6 pkg/*.rpm
+bundle exec package_cloud push omnibus-serverspec/serverspec/ubuntu/trusty pkg/*.deb
