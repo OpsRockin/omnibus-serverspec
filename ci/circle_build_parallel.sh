024679f1f7d881b77d24aa638da6dec9337b8994
sawanoboly <sawanoboriyu@higanworks.com>
2015-01-16 17:54:08 +0900

    set BUILD_ITERATION to circle_build_parallel

--- ci/circle_build_parallel.sh
+++ ci/circle_build_parallel.sh
@@ -9,10 +9,11 @@ case $CIRCLE_NODE_INDEX in
 esac
 
 RUBYPATH="/opt/rubies/ruby-2.1.5/bin"
+BUILD_ITERATION=${CIRCLE_BUILD_NUM:-1}
 
 bundle exec kitchen destroy ${TARGET}
 bundle exec kitchen converge ${TARGET}
 
 bundle exec kitchen exec ${TARGET} -c "export PATH=${RUBYPATH}:\$PATH; gem install bundler --no-ri --no-rdoc"
 bundle exec kitchen exec ${TARGET} -c "export PATH=${RUBYPATH}:\$PATH; cd /home/vagrant/serverspec; bundle install --jobs=2 --verbose --binstubs --without development"
-bundle exec kitchen exec ${TARGET} -c "export PATH=${RUBYPATH}:\$PATH; cd /home/vagrant/serverspec; ./bin/omnibus build serverspec"
+bundle exec kitchen exec ${TARGET} -c "export PATH=${RUBYPATH}:\$PATH; export BUILD_ITERATION=${BUILD_ITERATION}; cd /home/vagrant/serverspec; ./bin/omnibus build serverspec"
