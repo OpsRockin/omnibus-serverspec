ded36f00a16ddadbb5ff8e19ae8ff7b6e19ad521
sawanoboly <sawanoboriyu@higanworks.com>
2015-02-19 15:12:52 +0900

    remove destroy

--- ci/circle_build_parallel.sh
+++ ci/circle_build_parallel.sh
@@ -11,9 +11,6 @@ esac
 RUBYPATH="/opt/rubies/ruby-2.1.5/bin"
 BUILD_ITERATION=${CIRCLE_BUILD_NUM:-1}
 
-bundle exec kitchen create ${TARGET}  ## When droplet exists, collect id from D.O.
-bundle exec kitchen destroy ${TARGET} ## and destroy it to clear cache.
-sleep 10 # wait destroying.
 sudo chown ubuntu.ubuntu ${HOME}/.vagrant.d
 bundle exec kitchen converge ${TARGET}
 
