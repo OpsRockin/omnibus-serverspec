684abbc74b1f612bbc95259a445005addf804416
sawanoboly <sawanoboriyu@higanworks.com>
2015-04-20 19:55:31 +0900

    set HOME for read git global config

--- ci/circle_build_parallel.sh
+++ ci/circle_build_parallel.sh
@@ -16,4 +16,4 @@ bundle exec kitchen converge ${TARGET}
 
 bundle exec kitchen exec ${TARGET} -c "export PATH=${RUBYPATH}:\$PATH; gem install bundler --no-ri --no-rdoc"
 bundle exec kitchen exec ${TARGET} -c "export PATH=${RUBYPATH}:\$PATH; cd /home/vagrant/serverspec; bundle install --jobs=2 --verbose --binstubs --without development"
-bundle exec kitchen exec ${TARGET} -c "export PATH=${RUBYPATH}:\$PATH; export BUILD_ITERATION=${BUILD_ITERATION}; cd /home/vagrant/serverspec; ./bin/omnibus build serverspec"
+bundle exec kitchen exec ${TARGET} -c "export PATH=${RUBYPATH}:\$PATH; export HOME=/home/vagrant ; export BUILD_ITERATION=${BUILD_ITERATION}; cd /home/vagrant/serverspec; ./bin/omnibus build serverspec"
