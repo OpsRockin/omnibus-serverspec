430e733f617ee965aaff414e071b25dfb4f19f45
sawanoboly <sawanoboriyu@higanworks.com>
2015-06-17 12:15:21 +0900

    remove build_version.

--- config/software/serverspec.rb
+++ config/software/serverspec.rb
@@ -5,7 +5,6 @@ require 'open-uri'
 s_versions = JSON.parse(open('https://rubygems.org/api/v1/versions/serverspec.json').read)
 s_versions.select! {|r| r.has_key?('number')}
 default_version s_versions.first['number'] # Rubygems Option
-build_version s_versions.first['number'] # Build Option
 gem_version = s_versions.first['number']   # local_variable
 
 #source :git => "https://github.com/serverspec/serverspec.git"
