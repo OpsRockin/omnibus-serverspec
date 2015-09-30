d1aee6e57a92262e6918655382ea9321c908d015
sawanoboly <sawanoboriyu@higanworks.com>
2015-09-30 20:38:23 +0900

    try release parallely

--- ci/circle_release.sh
+++ ci/circle_release.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
 set -e
-rake release_to_packagecloud[$CIRCLE_BRANCH,el/6,'rpm']
-rake yank_oldest_release[$CIRCLE_BRANCH,el/6]
-
-rake release_to_packagecloud[$CIRCLE_BRANCH,el/7,'rpm']
-rake yank_oldest_release[$CIRCLE_BRANCH,el/7]
-
-rake release_to_packagecloud[$CIRCLE_BRANCH,ubuntu/trusty,'deb']
-rake yank_oldest_release[$CIRCLE_BRANCH,ubuntu/trusty]
+rake release_to_packagecloud[$CIRCLE_BRANCH,$1,$2]
+rake yank_oldest_release[$CIRCLE_BRANCH,$1]
