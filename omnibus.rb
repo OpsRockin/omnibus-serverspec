d9ea5f95596d9e0757b6e58eaf28625b00517259
sawanoboly <sawanoboriyu@higanworks.com>
2015-09-30 15:14:00 +0900

    check on vm

--- omnibus.rb
+++ omnibus.rb
@@ -1,18 +1,7 @@
-cache_dir              './local/omnibus/cache'
-git_cache_dir './local/omnibus/cache/install_path'
-source_dir             './local/omnibus/src'
-build_dir              './local/omnibus/build'
-package_dir            './local/omnibus/pkg'
-package_tmp            './local/omnibus/pkg-tmp'
+cache_dir              File.expand_path('../local/omnibus/cache', __FILE__)
+git_cache_dir          File.expand_path('../local/omnibus/cache/install_path', __FILE__)
+source_dir             File.expand_path('../local/omnibus/src', __FILE__)
+build_dir              File.expand_path('../local/omnibus/build', __FILE__)
+package_dir            File.expand_path('../local/omnibus/pkg', __FILE__)
+package_tmp            File.expand_path('../local/omnibus/pkg-tmp', __FILE__)
 append_timestamp false
-
-if ENV['AWS_ACCESS_KEY_ID'] and ENV['AWS_SECRET_ACCESS_KEY'] and ENV['S3_BUCKET']
-  s3_access_key ENV['AWS_ACCESS_KEY_ID']
-  s3_secret_key ENV['AWS_SECRET_ACCESS_KEY']
-  s3_bucket ENV['S3_BUCKET']
-  use_s3_caching true
-  publish_s3_access_key ENV['AWS_ACCESS_KEY_ID']
-  publish_s3_secret_key ENV['AWS_SECRET_ACCESS_KEY']
-end
-# solaris_compiler "gcc"
-# build_retries 3
