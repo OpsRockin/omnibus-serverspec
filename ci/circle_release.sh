316de6a486fb9c4e0caf5d8c6342476571b5f7b8
sawanoboly <sawanoboriyu@higanworks.com>
2016-03-09 22:10:50 +0900

    renew tasknames

--- ci/circle_release.sh
+++ ci/circle_release.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 set -e
-rake release_to_packagecloud[$CIRCLE_BRANCH,$1,$2]
-rake yank_oldest_release[$CIRCLE_BRANCH,$1]
+rake ackagecloud:release[$CIRCLE_BRANCH,$1,$2]
+rake packagecloud:yank_oldest[$CIRCLE_BRANCH,$1]
 
 rake bintray:release
 rake bintray:yank_oldest
